2f2ee1437cb65f9c93a241c6ec82da872be379ae
Itamar Curiel <itamarcu@gmail.com>
2017-12-21 07:58:05 +0200

    - again

--- day_21.py
+++ day_21.py
@@ -4,6 +4,11 @@ from numpy import *
 def day_21():
     with open("input.txt") as file:
         lines = file.readlines()
+
+    def tup(matrix_2d):
+        return tuple(map(tuple, matrix_2d))
+    def untup(tup_2d):
+        return asmatrix(array(tup_2d))
     # .#./###/.#. => #..#/.###/..#./.##.
     # ##/#. => .#./.../...
     rules = dict()
@@ -11,12 +16,13 @@ def day_21():
         rule = [[list(p) for p in part.split("/")] for part in
                 line.strip().split(" => ")]
         mats = [asmatrix(array(rulepart)) for rulepart in rule]
+        m1 = tup(mats[1])
         for j in range(4):
-            rules[rot90(mats[0], j, (0, 1))] = mats[1]
-            rules[rot90(flip(mats[0], 0), j, (0, 1))] = mats[1]
+            rules[tup(rot90(mats[0], j, (0, 1)))] = m1
+            rules[tup(rot90(flip(mats[0], 0), j, (0, 1)))] = m1
     
     def part_1():
-        mat = asmatrix(array([list(".#."), list("..#"), list("###")]))
+        mat = tup(asmatrix(array([list(".#."), list("..#"), list("###")])))
         n = 3
         for _ in range(5):
             if n % 2 == 0:
@@ -25,7 +31,8 @@ def day_21():
                     for y in range(0, n, 2):
                         submat = mat[y:y+2][x:x+2]
                         newmat_arr[x//2][y//2] = rules[submat]
-        print(mat.flatten())
+                mat = tup(newmat_arr)
+        print(untup(mat).flatten())
         pass
     
     def part_2():
