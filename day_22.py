fc1216385283ec93d8ea34362c9ef320d0cd59c0
Itamar Curiel <itamarcu@gmail.com>
2017-12-23 06:56:06 +0200

    edit day 22 to include graph

--- day_22.py
+++ day_22.py
@@ -1,4 +1,7 @@
 from collections import defaultdict
+import cmath
+
+import numpy as np
 
 
 def day_22():
@@ -27,7 +30,7 @@ def day_22():
                 infecteds.add(current)
                 count += 1
             current += direction
-                
+        
         print("Part 1:", count)
     
     def part_2():
@@ -38,11 +41,12 @@ def day_22():
             for x in range(N):
                 if lines[y][x] == '#':
                     states[x - N // 2 + (-y + N // 2) * 1j] = 2
-    
+        
         current = 0 + 0j
         direction = 1j
-    
+        
         count = 0
+        m1 = M1 = mj = Mj = 0
         for _ in range(10000000):
             state = states[current]
             if state == 0:
@@ -56,10 +60,25 @@ def day_22():
             if state == 3:
                 del states[current]
             else:
-                states[current] = (states[current] + 1) % 4  # states are only 0..3
+                states[current] = (states[
+                                       current] + 1) % 4  # states are only 0..3
             current += direction
-    
+            m1 = min(m1, current.real)
+            M1 = max(M1, current.real)
+            mj = min(mj, current.imag)
+            Mj = max(Mj, current.imag)
+        
         print("Part 2:", count)
+        
+        from pylab import imshow, show
+        M = 250
+        Z = np.zeros((2*M, 2*M))
+        for y in range(-M, M):
+            for x in range(-M, M):
+                Z[x + M, y + M] = states[x + y * 1j]
+        
+        imshow(Z)
+        show()
     
     part_1()
     part_2()
